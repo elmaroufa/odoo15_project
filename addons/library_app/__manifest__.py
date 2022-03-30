b00813f7cf145193c163e1fb26aa18a56c2c0018
elmaroufa <elmaroufa@gmail.com>
2022-03-30 21:55:10 +0100

    add security rule and csv file

--- addons/library_app/__manifest__.py
+++ addons/library_app/__manifest__.py
@@ -8,6 +8,7 @@
     "depends": ["base"],
     "data": [
          "security/library_security.xml",
+         "security/ir.model.access.csv",
          "views/library_menu.xml",
         ],
     "application": True,
